8a3140555d8e9b89c1b76c9f4c4916c659957ecc
Vincent Boulaye <vincent.boulaye@orange.com>
2021-12-01 07:00:11 +0100

    first try

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,39 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var counter =0
+        var last = Integer.MAX_VALUE
+        for (s in input) {
+            if(last< Integer.valueOf(s)) {
+                counter++
+
+            }
+
+            last=Integer.valueOf(s)
+        }
+        return counter
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val integers = input.map { it -> Integer.valueOf(it) }
+        var counter =0
+        var last = Integer.MAX_VALUE
+        for ((index, s) in integers.withIndex()) {
+            if(index<2) continue
+            var newLast = integers.get(index)+integers.get(index-1)+integers.get(index-2)
+            if(last< newLast) {
+                counter++
+
+            }
+
+            last=newLast
+        }
+        return counter
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
