55a61501e164472f0b91b6ebbab80ff8bda8b5a0
Vincent Boulaye <vincent.boulaye@orange.com>
2021-12-01 07:25:03 +0100

    kotlin way

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,33 +1,43 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        var counter =0
-        var last = Integer.MAX_VALUE
-        for (s in input) {
-            if(last< Integer.valueOf(s)) {
-                counter++
+
+    data class Ctx(
+        val last: Int = Integer.MAX_VALUE,
+        val counter: Int = 0
+    ) {
+        fun next(newVal: Int): Ctx = when {
+            newVal > last -> Ctx(newVal, counter + 1)
+            else -> Ctx(newVal, counter)
+        }
+    }
+
+    fun part1Clean(input: List<String>): List<Int> {
+        return input.map(String::toInt)
+    }
+
+    fun part1Work(input: List<Int>): Int {
+        return input
+            .fold(Ctx()) { a, v -> a.next(v) }
+            .counter
+    }
+
+    fun part2Work(input: List<Int>): Int {
+        return input
+            .foldIndexed(Ctx()) { i, a, v ->
+                when {
+                    i < 2 -> a
+                    else -> a.next(v + input[i - 1] + input[i - 2])
+                }
 
             }
+            .counter
+    }
 
-            last=Integer.valueOf(s)
-        }
-        return counter
+    fun part1(input: List<String>): Int {
+        return part1Work(part1Clean(input))
     }
 
     fun part2(input: List<String>): Int {
-        val integers = input.map { it -> Integer.valueOf(it) }
-        var counter =0
-        var last = Integer.MAX_VALUE
-        for ((index, s) in integers.withIndex()) {
-            if(index<2) continue
-            var newLast = integers.get(index)+integers.get(index-1)+integers.get(index-2)
-            if(last< newLast) {
-                counter++
-
-            }
-
-            last=newLast
-        }
-        return counter
+        return part2Work(part1Clean(input))
     }
 
     // test if implementation meets criteria from the description, like:
--- src/Utils.kt
+++ src/Utils.kt
@@ -7,6 +7,7 @@ import java.security.MessageDigest
  */
 fun readInput(name: String) = File("src", "$name.txt").readLines()
 
+
 /**
  * Converts string to md5 hash.
  */
