ad8958caa6354fb731ecca5e632768f96bff8671
Vincent Boulaye <vincent.boulaye@orange.com>
2021-12-02 07:18:57 +0100

    refactor

--- src/day02/Puzzle.kt
+++ src/day02/Puzzle.kt
@@ -1,20 +1,25 @@
 package day02
 
 import utils.readInput
-import java.lang.IllegalStateException
 
 data class Ctx(
-    val depth: Int=0,
-    val pos: Int=0
+    val depth: Int = 0,
+    val pos: Int = 0,
+    val aim: Int = 0,
 ) {
 
-    fun move(action: Move): Ctx {
-        return when {
-            action.dir.startsWith("f") -> Ctx(depth, pos + action.mov)
-            action.dir.startsWith("u") -> Ctx(depth - action.mov, pos)
-            action.dir.startsWith("d") -> Ctx(depth + action.mov, pos)
-            else -> throw IllegalStateException();
+    fun next(move: Move): Ctx = move.let { (dir, mov) ->
+        return when (dir) {
+            Direction.forward -> Ctx(depth, pos + mov)
+            Direction.up -> Ctx(depth - mov, pos)
+            Direction.down -> Ctx(depth + mov, pos)
         }
+    }
+
+    fun next2(move: Move): Ctx = when (move.dir) {
+        Direction.forward -> Ctx(depth + aim * move.mov, pos + move.mov, aim)
+        Direction.up -> Ctx(depth, pos, aim - move.mov)
+        Direction.down -> Ctx(depth, pos, aim + move.mov)
     }
 
     fun result(): Int {
@@ -22,46 +27,50 @@ data class Ctx(
     }
 }
 
-data class Move(val dir: String, val mov: Int)
+enum class Direction {
+    forward, down, up
+}
+
+data class Move(val dir: Direction, val mov: Int)
 
 class Puzzle {
 
-    fun part1(input: List<String>): Int {
-        return part1Work(part1Clean(input))
+    fun clean(input: List<String>): List<Move> {
+        return input
+            .map { line -> line.split(" ") }
+            .map { (rawDir, rawMove) -> Move(Direction.valueOf(rawDir), rawMove.toInt()) }
     }
 
+    fun part1(rawInput: List<String>): Int {
+        val input = clean(rawInput)
+        return input
+            .fold(Ctx()) { acc, move -> acc.next(move) }
+            .result()
+    }
 
-    fun part2(input: List<String>): Int {
-        return part2Work(part1Clean(input))
+    fun part2(rawInput: List<String>): Int {
+        val input = clean(rawInput)
+        return input
+            .fold(Ctx()) { acc, move -> acc.next2(move) }
+            .result()
     }
 
 }
 
-fun part1Clean(input: List<String>): List<Move> {
-    return input
-        .map { x -> x.split(" ") }
-        .map { splited -> Move(splited[0], splited[1].toInt()) }
-}
-
-fun part1Work(input: List<Move>): Int {
-    return input.fold(Ctx()) {acc, move ->  acc.move(move)}.result()
-
-}
-
-fun part2Work(input: List<Move>): Int {
-    return input.size
-}
 
 fun main() {
     val puzzle = Puzzle()
     println(Puzzle::class.qualifiedName)
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("test", Puzzle::class)
-    check(puzzle.part1(testInput) == 150)
-    //check(puzzle.part2(testInput) == 0)
-
     val input = readInput("data", Ctx::class)
-    println(puzzle.part1(input))
-    println(puzzle.part2(input))
+
+    println("test1: ${puzzle.part1(testInput)}")
+    println("part1: ${puzzle.part1(input)}")
+    check(puzzle.part1(testInput) == 150)
+
+    println("test2: ${puzzle.part2(testInput)}")
+    println("part2: ${puzzle.part2(input)}")
+    check(puzzle.part2(testInput) == 900)
+
 }
