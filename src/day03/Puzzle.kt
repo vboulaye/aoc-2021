73f357895813beac8d57e312df60b97b8e5ae502
Vincent Boulaye <vincent.boulaye@orange.com>
2021-12-03 07:11:59 +0100

    raw

--- src/day03/Puzzle.kt
+++ src/day03/Puzzle.kt
@@ -2,8 +2,22 @@ package day03
 
 import utils.readInput
 
-class Ctx(
-    val value: Int = 0,
+fun matchesDigit(c: Char, c1: Char): Byte {
+    return if (c == c1) 1 else 0;
+}
+
+data class Result(val gamma: Int, val epsilon: Int = 0)
+data class Popularity(val one: Int = 0, val zero: Int = 0) {
+    fun mergeRecord(index: Int, char: Char): Popularity {
+      return  Popularity(
+            this.one + matchesDigit(char, '1'),
+            this.zero + matchesDigit(char, '0')
+        )
+    }
+}
+
+data class Ctx(
+    val values: List<Popularity>,
 //    val depth: Int = 0,
 //    val pos: Int = 0,
 //    val aim: Int = 0,
@@ -30,9 +44,38 @@ class Ctx(
 //        Direction.down -> Ctx(depth, pos, aim + move.mov)
 //    }
 
+
     fun result(): Int {
-        return 0;//depth * pos
+        val transformMost: (Popularity) -> Int = { if (it.one > it.zero) 1 else 0 }
+        val transformLeast: (Popularity) -> Int = { if (it.one > it.zero) 0 else 1 }
+        val map = this.values.map(transformMost)
+        val map2 = this.values.map(transformLeast)
+        var gamma: String = ""
+        for (i in map) {
+            gamma += i
+        }
+
+        var epsilon: String = ""
+        for (i in map2) {
+            epsilon += i
+        }
+
+        return gamma.toInt(2) * epsilon.toInt(2);//depth * pos
     }
+
+    fun merge(move: String): Ctx {
+
+        val mergeRecord: (index: Int, Char) -> Popularity = { index, c ->
+            Popularity(
+                this.values[index].one + matchesDigit(c, '1'),
+                this.values[index].zero + matchesDigit(c, '0')
+            )
+        }
+        val mapIndexed = move.mapIndexed(mergeRecord)
+        return Ctx(mapIndexed)
+    }
+
+
 }
 
 enum class Direction {
@@ -46,28 +89,58 @@ class Move(
 
 class Puzzle {
 
-    fun clean(input: List<String>): List<Move> {
+    fun clean(input: List<String>): List<String> {
         return input
-            .map { x -> x.toInt() }
-            .map { x -> Move() }
+//            .map { x -> x.toInt() }
+//            .map { x -> Move() }
 //            .map { line -> line.split(" ") }
 //            .map { (rawDir, rawMove) -> Move(Direction.valueOf(rawDir), rawMove.toInt()) }
     }
 
     fun part1(rawInput: List<String>): Int {
         val input = clean(rawInput)
-        return input
-            .fold(Ctx()) { acc, move -> acc.next(move) }
+
+        val init = List(input[0].length) { Popularity() }
+        val fold = input.fold(Ctx(init))
+        { acc, move -> acc.merge(move) }
+        return fold
+//            .fold(Ctx()) { acc, move -> acc.next(move) }
             .result()
     }
 
     fun part2(rawInput: List<String>): Int {
         val input = clean(rawInput)
-        return input
-            .fold(Ctx()) { acc, move -> acc.next2(move) }
-            .result()
+
+        val oxygen = processs(input, 0, true)
+        val co2 = processs(input, 0, false)
+
+        val toInt = oxygen.toInt(2)
+        val toInt1 = co2.toInt(2)
+        return toInt * toInt1
+        //    .fold(Ctx()) { acc, move -> acc.next2(move) }
+        //     .result()
     }
 
+     tailrec fun processs(input: List<String>, index: Int, mode: Boolean): String {
+        val popularityForIndexedValue = input.fold(Popularity()) { acc, record -> acc.mergeRecord(index, record[index]) }
+        val matchingChar = if(mode) {
+            if(popularityForIndexedValue.one>=popularityForIndexedValue.zero) '1' else '0'
+        } else {
+            if(popularityForIndexedValue.one==popularityForIndexedValue.zero)  '0'
+            if(popularityForIndexedValue.one<popularityForIndexedValue.zero) '1' else '0'
+        }
+        val filtered = input.filter { it[index] == matchingChar }
+        if(filtered.size==1) {return filtered[0]}
+        else {
+           return  processs(filtered, index+1, mode)
+        }
+    }
+
+}
+
+private fun <E> List<E>.merge(move: String) {
+    move.mapIndexed { index, c -> }
+    TODO("Not yet implemented")
 }
 
 
@@ -78,13 +151,13 @@ fun main() {
     val testInput = readInput("00test", Puzzle::class)
     val input = readInput("zzdata", Ctx::class)
 
-    val part1ExpectedResult = 0
+    val part1ExpectedResult = 198
 
     println("test1: ${puzzle.part1(testInput)} == $part1ExpectedResult")
     println("part1: ${puzzle.part1(input)}")
     check(puzzle.part1(testInput) == part1ExpectedResult)
 
-    val part2ExpectedResult = 0
+    val part2ExpectedResult = 230
 
     println("test2: ${puzzle.part2(testInput)} == $part2ExpectedResult")
     println("part2: ${puzzle.part2(input)}")
