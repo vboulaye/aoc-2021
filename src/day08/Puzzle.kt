7b35e1294a83843abd18be4764362085a183c18e
Vincent Boulaye <vincent.boulaye@orange.com>
2021-12-08 09:04:21 +0100

    refactor

--- src/day08/Puzzle.kt
+++ src/day08/Puzzle.kt
@@ -36,9 +36,9 @@ class Puzzle {
         return input.map {
             val digitcodes = it.split(" | ")
             check(digitcodes.size == 2)
-            val digits = digitcodes[0].split(" ").map { it.toList().sorted().joinToString ("") }
+            val digits = digitcodes[0].split(" ").map { it.toList().sorted().joinToString("") }
             check(digits.size == 10)
-            val code = digitcodes[1].split(" ").map { it.toList().sorted().joinToString ("") }
+            val code = digitcodes[1].split(" ").map { it.toList().sorted().joinToString("") }
             check(code.size == 4)
             Entry(digits, code)
         }
@@ -47,22 +47,15 @@ class Puzzle {
     val part1ExpectedResult = 26L
     fun part1(rawInput: List<String>): Result {
         val input = clean(rawInput)
-
         return input.map { it.code.fold(0) { acc, s -> acc + if (isUnique(s)) 1 else 0 } }.sum().toLong()
     }
-
-//    data class Trad(
-//        val List<List<Int>>
-//    ) {
-//
-//    }
 
     private fun isUnique(s: String): Boolean {
         return when (s.length) {
             2, 3, 4, 7 -> true  // (1,4,7,8)
             5 -> false //2 3 5
             6 -> false // 0 6 9
-            else -> false //0 2 3 5 6 9
+            else -> throw IllegalStateException()
         }
 
     }
@@ -81,172 +74,90 @@ class Puzzle {
 
     }
 
-    private fun possibleSegements(s: String): Map<Char, List<Char>> {
-        return when (s.length) {
-            2 -> possible(s, "cf") // (1,4,7,8)
-            3 -> possible(s, "acf")  // (1,4,7,8)
-            4 -> possible(s, "bcdf")  // (1,4,7,8)
-            7 -> possible(s, "abcdefg")  // (1,4,7,8)
-            5 -> possible(s, "acdegacdfgabdfg") //2 3 5
-            6 -> possible(s, "abcefgabdefgabcdfg") // 0 6 9
-            else -> throw IllegalStateException()
+
+    fun extractMatchingEncodedDigit(
+        encodedDigitToPossibleDigits: Map<String, List<Int>>,
+        predicates: List<(Map.Entry<String, List<Int>>) -> Boolean>
+    ): String {
+
+        val matchingList = encodedDigitToPossibleDigits.filter { encodedDigitToPossibleDigit ->
+            predicates.all { predicate ->
+                predicate(encodedDigitToPossibleDigit)
+            }
         }
-
+            .toList()
+        check(matchingList.size == 1) { "no exact match $matchingList" }
+        return matchingList[0].first
     }
-
-    private fun possible(s: String, mapped: String): Map<Char, List<Char>> {
-        return s.map { it to mapped.toList().toSet().toList().sorted() }.toMap()
-    }
-
-    private fun trad(s: String): Int {
-        return when (s) {
-            "cf" -> 1
-            "abcefg" -> 0
-            "acdeg" -> 2
-            "acdfg" -> 3
-            "bcdf" -> 4
-            "abdfg" -> 5
-            "abdefg" -> 6
-            "acf" -> 7
-            "abcdefg" -> 8
-            "abcdfg" -> 9
-            else -> throw IllegalStateException()
-        }
-
-    }
-
-    val dic = mapOf(
-        "acedgfb" to 8,
-        "cdfbe" to 5,
-        "gcdfa" to 2,
-        "fbcad" to 3,
-        "dab" to 7,
-        "cefabd" to 9,
-        "cdfgeb" to 6,
-        "eafb" to 4,
-        "cagedb" to 0,
-        "ab" to 1,
-    )
-    val numberToString: Map<Int, String> = mapOf(
-        8 to "acedgfb",
-        5 to "cdfbe",
-        2 to "gcdfa",
-        3 to "fbcad",
-        7 to "dab",
-        9 to "cefabd",
-        6 to "cdfgeb",
-        4 to "eafb",
-        0 to "cagedb",
-        1 to "ab",
-    )
 
     val part2ExpectedResult = 61229L
     fun part2(rawInput: List<String>): Result {
         val input = clean(rawInput)
-        val codes = input.map {
+        val codes = input.map { encodedDigitsAndCode ->
 
-            val codeDigitToPossibleDigits = HashMap(it.digits.map {
-                it to possibleDigits(it)
-            }.toMap())
+            val encodedDigitToPossibleDigits: java.util.HashMap<String, List<Int>> =
+                HashMap(encodedDigitsAndCode.digits.map {
+                    it to possibleDigits(it)
+                }.toMap())
 
-            val digitToCodeDigit = HashMap(codeDigitToPossibleDigits
-                .filter { it.value.size == 1 }
+            val digitToCodeDigit = HashMap(encodedDigitToPossibleDigits
+                .filter({ (encodedDigit, possibleDigits) -> possibleDigits.size == 1 })
                 .map { it.value[0] to it.key }
                 .toMap()
             )
-            digitToCodeDigit.values.forEach { codeDigitToPossibleDigits.remove(it) }
+            digitToCodeDigit.values.forEach { encodedDigitToPossibleDigits.remove(it) }
 
-            // 1 4 7 8
-//            5 -> possible(s, "acdegacdfgabdfg") //2 3 5
-//            6 -> possible(s, "abcefgabdefgabcdfg") // 0 6 9
+            digitToCodeDigit[3] = extractMatchingEncodedDigit(
+                encodedDigitToPossibleDigits,
+                listOf(
+                    { it.key.length == 5 } ,
+                    { isIncluded(it.key, digitToCodeDigit[1]!!) } ,
+                )
+            )
+            digitToCodeDigit.values.forEach { encodedDigitToPossibleDigits.remove(it) }
 
+            digitToCodeDigit[9] = extractMatchingEncodedDigit(
+                encodedDigitToPossibleDigits,
+                listOf(
+                    { it.key.length == 6 } ,
+                    { isIncluded(it.key, digitToCodeDigit[4]!!) } ,
+                )
+            )
+            digitToCodeDigit.values.forEach { encodedDigitToPossibleDigits.remove(it) }
 
-            val threeList = codeDigitToPossibleDigits
-                .filter { (it.key.length == 5) }
-                .filter { isIncluded(it.key, digitToCodeDigit[1]!!) }
-                .toList()
-            digitToCodeDigit[3] = threeList[0].first
-            digitToCodeDigit.values.forEach { codeDigitToPossibleDigits.remove(it) }
-
-            val nineList = codeDigitToPossibleDigits
-                .filter { (it.key.length == 6) }
-                .filter { isIncluded(it.key, digitToCodeDigit[4]!!) }
-                .toList()
-            digitToCodeDigit[9] = nineList[0].first
-            digitToCodeDigit.values.forEach { codeDigitToPossibleDigits.remove(it) }
-            val zeroList = codeDigitToPossibleDigits
+            val zeroList = encodedDigitToPossibleDigits
                 .filter { (it.key.length == 6) }
                 .filter { isIncluded(it.key, digitToCodeDigit[1]!!) }
                 .toList()
             digitToCodeDigit[0] = zeroList[0].first
-            digitToCodeDigit.values.forEach { codeDigitToPossibleDigits.remove(it) }
-            val sixList = codeDigitToPossibleDigits
+            digitToCodeDigit.values.forEach { encodedDigitToPossibleDigits.remove(it) }
+            val sixList = encodedDigitToPossibleDigits
                 .filter { (it.key.length == 6) }
                 .toList()
             digitToCodeDigit[6] = sixList[0].first
-            digitToCodeDigit.values.forEach { codeDigitToPossibleDigits.remove(it) }
+            digitToCodeDigit.values.forEach { encodedDigitToPossibleDigits.remove(it) }
 
-            val fiveList = codeDigitToPossibleDigits
+            val fiveList = encodedDigitToPossibleDigits
                 .filter { (it.key.length == 5) }
-                .filter { isIncluded(digitToCodeDigit[6]!!, it.key ) }
+                .filter { isIncluded(digitToCodeDigit[6]!!, it.key) }
                 .toList()
             digitToCodeDigit[5] = fiveList[0].first
-            digitToCodeDigit.values.forEach { codeDigitToPossibleDigits.remove(it) }
+            digitToCodeDigit.values.forEach { encodedDigitToPossibleDigits.remove(it) }
 
-            val twoList = codeDigitToPossibleDigits
+            val twoList = encodedDigitToPossibleDigits
                 .filter { (it.key.length == 5) }
                 .toList()
             digitToCodeDigit[2] = twoList[0].first
-            digitToCodeDigit.values.forEach { codeDigitToPossibleDigits.remove(it) }
+            digitToCodeDigit.values.forEach { encodedDigitToPossibleDigits.remove(it) }
 
             val codeDigitToDigit = digitToCodeDigit.map { it.value to it.key }.toMap()
 
-//            val map1 = it.digits.map { possibleSegements(it) }
-//            val possibleCharDic: MutableMap<Char, List<Char>> = mutableMapOf(
-//
-//            )
-//            map1.forEach {
-//                it.forEach { (digit: Char, possibilities: List<Char>) ->
-//                    val chars = possibleCharDic[digit]
-//                    if (chars == null) {
-//                        possibleCharDic[digit] = possibilities
-//                    } else {
-//                        possibleCharDic[digit] = possibilities.intersect(chars).toList().sorted()
-//                    }
-//                }
-//                possibleCharDic
-//            }
-//            data class Mapper(val digit: Int, val size: Int, val possibleDigits: List<Char>)
-//
-//            val mappers = numberToString.map { it ->
-//                val wishedString = it.value
-//                val number = it.key
-//                val flatMap: List<Char> =
-//                    wishedString.fold("abcdefg".toList()) { acc, it -> acc.intersect(possibleCharDic[it]!!).toList() }
-//                        .toSet().toList().sorted()
-//                Mapper(number, wishedString.length, flatMap)
-//            }
-//be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
-            val codeValue = it.code.map { character ->
+            val codeValue = encodedDigitsAndCode.code.map { character ->
 
                 //System.err.println(    codeDigitToDigit[character])
                 val i = codeDigitToDigit[character]
-                 i!!.toString()
-//                val filter = mappers.filter { mapper ->
-//                    mapper.size == character.length
-//                            && character.toList()
-//                        .filter {
-//                            val possibleSegmentsForCodeSegment = possibleCharDic[it]!!
-//                            val neededSegments = numberToString[mapper.digit]!!.toList()
-//                            !possibleSegmentsForCodeSegment.intersect(neededSegments).isEmpty()
-//                        }
-//                        // .intersect(mapper.possibleDigits)
-//                        .size == mapper.size
-//                }
-//                check(filter.size == 1)
-//                val mapped = filter[0]
-//                check(mapped != null)
-//                mapped
+                i!!.toString()
+
             }.joinToString("")
             codeValue.toInt()
         }
