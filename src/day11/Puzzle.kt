84cf6b9b42c90d8e3a9b9174b36b66e6b0e03a60
Vincent Boulaye <vincent.boulaye@orange.com>
2021-12-11 06:55:19 +0100

    raw

--- src/day11/Puzzle.kt
+++ src/day11/Puzzle.kt
@@ -1,7 +1,6 @@
 package day11
 
 import utils.readInput
-import java.math.BigInteger
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTime
 
@@ -10,21 +9,105 @@ typealias Result = Long
 
 class Puzzle {
 
-    fun clean(input: List<String>): List<String> {
-        return input.map { it }
+    fun clean(input: List<String>): List<List<Int>> {
+        return input.map { it.toList().map { it.code - '0'.code } }
     }
 
-    val part1ExpectedResult = 0L
+    val part1ExpectedResult = 1656L
     fun part1(rawInput: List<String>): Result {
         val input = clean(rawInput)
-        return 0;
+        var count: Long = 0
+        (0..99).fold(input) { acc, it ->
+            val newState: List<List<Int>> = step(acc)
+            val flashes = countFlashes(newState)
+            count += flashes
+            newState
+        }
+
+        return count;
     }
 
-    val part2ExpectedResult = 0L
+    private fun step(input: List<List<Int>>): List<List<Int>> {
+
+
+        val newState = input.map { line ->
+            line.map { octopus ->
+                octopus + 1
+            }.toMutableList()
+        }.toMutableList()
+
+        val hasFlashed = input.map { line ->
+            line.map { octopus ->
+                false
+            }.toMutableList()
+        }.toMutableList()
+
+        //    val printed=         newState.joinToString("\n") {  it.joinToString { it.toString() } }
+        //  System.err.println(printed        +"\n")
+
+        newState.forEachIndexed { y, line ->
+            line.forEachIndexed { x, octopus ->
+                if (octopus > 9 && !hasFlashed[y][x]) {
+                    hasFlashed[y][x] = true
+                    flashAround(y, x, newState, hasFlashed)
+                }
+
+            }
+        }
+        val newsState2 = newState.map { line ->
+            line.map { octopus ->
+                if (octopus > 9) 0 else octopus
+            }
+        }
+        //val printed2=         newsState2.joinToString("\n") {  it.joinToString { it.toString() } }
+        //  System.err.println(printed2        +"\n")
+        return newsState2
+    }
+
+    private fun flashAround(
+        y: Int,
+        x: Int,
+        newState: MutableList<MutableList<Int>>,
+        hasFlashed: MutableList<MutableList<Boolean>>
+    ) {
+        (-1..1).forEach { dy ->
+            (-1..1).forEach { dx ->
+                val testX = x + dx
+                val testY = y + dy
+                if (!(dx == 0 && dy == 0)) {
+                    try {
+                        val i = newState[testY][testX]
+                        newState[testY][testX] = i + 1
+                        if (newState[testY][testX] > 9 && !hasFlashed[testY][testX]) {
+                            hasFlashed[testY][testX] = true
+                            flashAround(testY, testX, newState, hasFlashed)
+                        }
+                    } catch (e: Exception) {
+
+                    }
+                }
+            }
+
+        }
+    }
+
+    val part2ExpectedResult = 195L
     fun part2(rawInput: List<String>): Result {
         val input = clean(rawInput)
-        return 0;
+        var count: Long = 0
+        var index = 0
+        var newState = input
+        while (countFlashes(newState) != 100) {
+
+             newState= step(newState)
+            index++
+        }
+
+        return index.toLong();
     }
+
+    private fun countFlashes(newState: List<List<Int>>) =
+        newState.sumOf { line: List<Int> -> line.count { it == 0 } }
 
 }
 
