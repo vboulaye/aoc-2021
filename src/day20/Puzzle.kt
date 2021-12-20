7a233e9fdf9825f4985070a7c76798d5fc7f3a63
Vincent Boulaye <vincent.boulaye@orange.com>
2021-12-20 18:32:13 +0100

    refactor

--- src/day20/Puzzle.kt
+++ src/day20/Puzzle.kt
@@ -10,55 +10,60 @@ typealias Result = Long
 
 data class Input(val algo: List<Char>, val grid: List<List<Char>>, val defaultChar: Char = '.') {
     val s = grid.joinToString("\n") { it.joinToString("") }
+    override fun toString(): String = s
 
-
-    private fun makeEmptyLine(wished: Int) = (0 until wished).map { defaultChar }
+    private fun makeEmptyLine(wished: Int) = List(wished) { defaultChar }
 
     fun enhance(): Input {
-        val newDots = 2
-        val newLine: List<List<Char>> = (0 until newDots).map { makeEmptyLine(grid[0].size + 2 * newDots) }
-        val addColumns: List<List<Char>> = grid.map {
-            makeEmptyLine(newDots) + it + makeEmptyLine(newDots)
-        }
-        val newGrid: List<List<Char>> = newLine + addColumns + newLine
-
-        val mutable = newGrid.map { it.toMutableList() }.toMutableList()
+        val newGrid: List<List<Char>> = expandGrid(2)
         val enhanced = newGrid.mapIndexed { y, line ->
-            line.mapIndexed { x, c ->
-                val w = getWeight(x, y, mutable)
-                algo[w.asBinary()].toChar()
+            line.mapIndexed { x, _ ->
+                algo[getWeight(x, y, newGrid).asBinary()]
             }
         }
         val newDef: Char = if (defaultChar == '.') {
-            algo[0]
+            algo["0".repeat(9).asBinary()]
         } else {
-            algo["111111111".asBinary()]
+            algo["1".repeat(9).asBinary()]
         }
         return Input(algo, enhanced, newDef)
     }
 
-    private fun getWeight(x: Int, y: Int, mutable: MutableList<MutableList<Char>>): String {
-        return (-1..1).map { dy ->
-            (-1..1).map { dx ->
-                try {
-                    mutable[y + dy][x + dx]
-                } catch (e: IndexOutOfBoundsException) {
-                    defaultChar
-                }
-            }.map { if (it == '.') '0' else '1' }
-
+    private fun expandGrid(newDots: Int): List<List<Char>> {
+        val newLine: List<List<Char>> = List(newDots) { makeEmptyLine(grid.size + 2 * newDots) }
+        val addColumns: List<List<Char>> = grid.map {
+            makeEmptyLine(newDots) + it + makeEmptyLine(newDots)
         }
-
-            .joinToString("")
-            { it.joinToString("") }
+        val newGrid: List<List<Char>> = newLine + addColumns + newLine
+        return newGrid
     }
+
+    private fun getWeight(x: Int, y: Int, grid: List<List<Char>>): String {
+        val viewDimension = -1..1
+        val view = viewDimension.map { dy ->
+            viewDimension.map { dx ->
+                get(y + dy, x + dx, grid)
+            }.map { if (it == '.') '0' else '1' }
+        }
+        val viewAsString = view.joinToString("") { it.joinToString("") }
+        return viewAsString
+    }
+
+    private fun get(y1: Int, x1: Int, grid: List<List<Char>>) = try {
+        grid[y1][x1]
+    } catch (e: IndexOutOfBoundsException) {
+        defaultChar
+    }
+
+    fun count() = grid.sumOf { line -> line.count { it == '#' } }.toLong()
+
 }
 
 class Puzzle {
     fun clean(input: List<String>): Input {
         val algo = input[0].toList()
-        val grid: List<List<Char>> = input.subList(2, input.size)
-            .filter { line -> true }
+        val grid: List<List<Char>> = input
+            .subList(2, input.size)
             .map { line -> line.toList() }
         return Input(algo, grid)
     }
@@ -66,27 +71,15 @@ class Puzzle {
     val part1ExpectedResult = 35L
     fun part1(rawInput: List<String>): Result {
         val input = clean(rawInput)
-
-//        val enhance1 = input.enhance();
-//        println("enhance1: \n${enhance1.s}")
-//        val enhance2 = enhance1.enhance();
-//        println("enhance2: \n${enhance2.s}")
-
         val enhanced = (0 until 2).fold(input) { acc, _ -> acc.enhance() }
-        val pixelCount = enhanced.grid.sumOf { line -> line.count { it == '#' } }.toLong()
-        // 5378 too low
-        // 6175 too hiugh
-        // 6782
-        // 6036 too high
-        return pixelCount
+        return enhanced.count()
     }
 
     val part2ExpectedResult = 3351L
     fun part2(rawInput: List<String>): Result {
         val input = clean(rawInput)
         val enhanced = (0 until 50).fold(input) { acc, _ -> acc.enhance() }
-        val pixelCount = enhanced.grid.sumOf { line -> line.count { it == '#' } }.toLong()
-        return pixelCount
+        return enhanced.count()
     }
 
 }
