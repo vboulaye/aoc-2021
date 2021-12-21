dc797ef84b26d85bd62e6727738283d02a05f0b8
Vincent Boulaye <vincent.boulaye@orange.com>
2021-12-21 10:30:51 +0100

    essai

--- src/day21/Puzzle.kt
+++ src/day21/Puzzle.kt
@@ -1,6 +1,9 @@
 package day21
 
 import utils.readInput
+import java.lang.Long.max
+import java.math.BigInteger
+import java.util.concurrent.atomic.AtomicLong
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTime
 
@@ -29,7 +32,27 @@ data class Player(val player: Int, var position: Int) {
     }
 }
 
+typealias Rolls = Int
 class Puzzle {
+    val possibleDiceScores: MutableMap<Int, Int> = mutableMapOf()
+
+    init {
+        (3..9).forEach { whishedScore ->
+
+            var counter = 0
+            (1..3).forEach { d1 ->
+                (1..3).forEach { d2 ->
+                    (1..3).forEach { d3 ->
+                        if (d1 + d2 + d3 == whishedScore) {
+                            counter++
+                        }
+                    }
+                }
+            }
+            possibleDiceScores[whishedScore] = counter
+        }
+    }
+
     fun clean(input: List<String>): List<Player> {
         return input
             .filter { line -> true }
@@ -58,24 +81,125 @@ class Puzzle {
         return (dice.value * (if (p1.score >= 1000) p2.score else p1.score)).toLong()
     }
 
+
     val part2ExpectedResult = 444356092776315L
     fun part2(rawInput: List<String>): Result {
         val input = clean(rawInput)
         val p1 = input[0]
         val p2 = input[1]
-        val dice = Dice(1, 3)
-        while (true) {
-            p1.move(dice)
-            if (p1.score >= 21) {
-                break;
-            }
-            p2.move(dice)
-            if (p2.score >= 21) {
-                break;
-            }
-        }
-        return (dice.value * (if (p1.score >= 1000) p2.score else p1.score)).toLong()
+
+
+//3 -> 1
+//        4-> 1+1+2 1+2+1 2+1+1 3
+
+//        5 ->1+1+3 / 1+3+1 /1+1+3/ 1+2+2/ 2+2+1 /2+1+2 6
+//        6 -> 3+2+1 /3+1+2 /2+3+1 / 2+1+3/ 2+2+2 /1+3+2/ 1+2+3  -> 7
+
+//        val newD1 = Dirac(1,p1.position, 0, emptyList(), ArrayDeque())
+//        val newD2 = Dirac(2, p2.position, 0, emptyList(), ArrayDeque())
+        val newD1 = Dirac(1, p1.position, 0)
+        val newD2 = Dirac(2, p2.position, 0)
+
+        playD1(newD1, newD2)
+//        val s1 = computeCount(newD1.games, possibleDiceScores)
+//        val s2 = computeCount(newD2.games, possibleDiceScores)
+
+//        while (true) {
+//            p1.move(dice)
+//            if (p1.score >= 21) {
+//                break;
+//            }
+//            p2.move(dice)
+//            if (p2.score >= 21) {
+//                break;
+//            }
+//        }
+//        return (dice.value * (if (p1.score >= 1000) p2.score else p1.score)).toLong()
+//        return max(s1, s2);
+        return max(newD1.globalCount.get(), newD2.globalCount.get());
     }
+
+    private fun computeWinngin(
+        player: Player,
+        possibleDiceScores: MutableMap<Int, Int>
+    ): Long {
+        val games1: ArrayDeque<List<Rolls>> = ArrayDeque()
+        computePossibleGames(player.position, games1)
+//        val distinct = games1.map { it.sorted() }.distinct()
+        return computeCount(games1, possibleDiceScores)
+    }
+
+    private fun computeCount(
+        games1: ArrayDeque<List<Rolls>>,
+        possibleDiceScores: MutableMap<Int, Int>
+    ): Long {
+        val score1 = games1.fold(BigInteger.ZERO) { acc, list ->
+            val fold: BigInteger = list.fold(BigInteger.ONE) { acc2, rolls ->
+                acc2 * possibleDiceScores[rolls]!!.toBigInteger()
+            }
+            acc + fold
+        }
+        return score1.toLong()
+    }
+
+    private fun computePossibleGames(
+        position: Int,
+        games: ArrayDeque<List<Rolls>>,
+        score: Int = 0,
+        rolls: List<Rolls> = emptyList()
+    ) {
+        if (score >= 21) {
+            games.add(rolls)
+            return
+        }
+        (3..9).forEach { rollsScore ->
+            val newPosition = position + rollsScore
+            val newScore = score + (newPosition + -1) % 10 + 1
+            possibleDiceScores[rollsScore]!!
+            computePossibleGames(newPosition, games, newScore, rolls + listOf(rollsScore))
+        }
+
+    }
+
+    data class Dirac(
+        val player: Int,
+        val position: Int,
+        val score: Int = 0,
+//        val rolls: List<Rolls> = emptyList(),
+//        val games: ArrayDeque<List<Rolls>>,
+        val currentCount: Long = 1,
+        var globalCount: AtomicLong = AtomicLong(0L),
+    )
+
+
+    private fun playD1(
+        d1: Dirac,
+        //  games: ArrayDeque<List<Rolls>>,
+        d2: Dirac
+    ) {
+        if (d1.score >= 21) {
+            //  println(d1.rolls)
+//            d1.games.add(d1.rolls)
+            d1.globalCount.set(d1.globalCount.get() + d1.currentCount)
+//            if(d1.games.size%100000==0) {
+//                println("${d1.player} ${d1.games.size}")
+//            }
+
+            return
+        }
+        (3..9).forEach { rollsScore ->
+            val newPosition = d1.position + rollsScore
+            val newScore = d1.score + (newPosition + -1) % 10 + 1
+            val newD1 = Dirac(
+                d1.player, newPosition, newScore, //d1.rolls + listOf(rollsScore), d1.games
+                d1.currentCount * possibleDiceScores[rollsScore]!!.toLong(),
+                d1.globalCount
+            )
+
+            playD1(d2, newD1)
+        }
+    }
+
 
 }
 
